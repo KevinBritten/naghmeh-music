1389d70247160b7977f68c70e4b07fd702d98220
Fern knee <kevinjbritten@gmail.com>
2021-09-15 20:19:18 -0400

    moved hamburger icon and header logo to new files

---
+++ src/components/HeaderLogo.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <img
+      class="header-logo"
+      src="../assets/naghmeh-old-logo-temp-resized.png"
+    />
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.header-logo {
+  position: fixed;
+  top: 10px;
+  left: 10px;
+  width: 60px;
+  z-index: 100;
+}
+</style>
