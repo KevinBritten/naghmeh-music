a5edf1b0da8a2c1843f26848c16a8dae90556647
Fern knee <kevinjbritten@gmail.com>
2021-11-11 02:32:18 -0500

    added vue sanity blocks component

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,43 +1,16 @@
 <template>
   <div>
-    <p>
-      Naghmeh Shafiei is an Iranian-Canadian singer/songwriter and the front
-      woman of the Montreal based post-modern rock band Naghmeh and the Southern
-      Shores. She began performing as a solo artist in 2016 while living in San
-      Francisco, deciding to abandon the corporate grind to pursue her dream of
-      becoming a touring musician. Naghmeh jumped head first into the world of
-      rock n’ roll and set off on the road over the following year with
-      performances across Europe and South America. Her DIY musical empire was
-      steadily growing as her music spread across the globe.
-    </p>
-    <p>
-      After years on the road and a few international collaborations, Naghmeh
-      moved back to her hometown of Montreal to focus on the next step of her
-      career. She assembled a band, performed in every corner of the city, and
-      recorded, co-produced and released her self-titled debut album in 2019.
-      Her sound is authentic in its delivery and blends different eras of music
-      into a punky, alternative, pop rock. With throaty melodies, satirical
-      lyrics and a touch of triumphant rock n' roll, Naghmeh's goal is to
-      transform the listener’s internal turmoil into a healing, whimsical dance.
-    </p>
-    <p>
-      She holds a Master of Arts in Psychology and has worked as a front woman,
-      song writer, event producer, performer, and session musician. She’s a
-      fierce entrepreneur and has worked hard her entire life to rise above the
-      prejudices she’s faced. Her story is inspiring and has just begun. Naghmeh
-      is on a mission to bring love and justice to the world and is doing so
-      through the art of music. She’s dedicating her efforts to produce her next
-      musical offering which is set to release in 2021. Stay tuned for Queen Of
-      Hearts!
-    </p>
+    <BlockContent :blocks="bio" />
   </div>
 </template>
 <script>
 import sanity from "../sanity";
+import BlockContent from "sanity-blocks-vue-component";
 
 const query = `*[_type == "information"]`;
 
 export default {
+  components: { BlockContent },
   data() {
     return {
       bio: [],
@@ -51,10 +24,8 @@ export default {
   methods: {
     fetchData() {
       this.error = this.post = null;
-
       return sanity.fetch(query).then(
         (information) => {
-          console.log(Array.from(information[0].bio));
           return information[0].bio;
         },
         (error) => {
