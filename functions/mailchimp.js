017f3a801116766fa26d4cf9c894627d13bb5cd0
Fern knee <kevinjbritten@gmail.com>
2022-01-28 04:22:33 -0500

    removed console logs

--- functions/mailchimp.js
+++ functions/mailchimp.js
@@ -3,13 +3,11 @@ const MC_API_KEY = process.env.MC_API_KEY;
 const MC_DATA_CENTER = MC_API_KEY.split("-")[1];
 const MC_AUDIENCE_ID = process.env.MC_AUDIENCE_ID;
 
-console.log(axios);
-console.log(MC_AUDIENCE_ID);
 const url = `https://${MC_DATA_CENTER}.api.mailchimp.com/3.0/lists/${MC_AUDIENCE_ID}/members`;
 
 exports.handler = async function(event, context) {
   const email = event.queryStringParameters.email;
-  console.log("email: " + email);
+  console.log("signing up with email: " + email);
   const data = {
     email_address: email,
     status: "subscribed"
@@ -31,10 +29,7 @@ exports.handler = async function(event, context) {
     .catch(() => {
       statusCode = 400;
     });
-  console.log("list:" + list);
-  console.log("statusCode: " + statusCode);
   return {
-    statusCode,
-    body: JSON.stringify({ message: "test" })
+    statusCode
   };
 };
