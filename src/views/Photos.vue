2d5d08ea1ef6599a4b22ee8ddf90a20df2db858b
Fern knee <kevinjbritten@gmail.com>
2022-03-10 20:53:07 -0500

    refactored photos into seperate component

--- src/views/Photos.vue
+++ src/views/Photos.vue
@@ -6,41 +6,21 @@
       :startingIndex="imageIndex"
       @closeLightbox="lightboxOpen = false"
     ></lightbox>
-    <div
-      class="album-container"
-      v-for="(album, albumIndex) in photoAlbums"
-      :key="album.title"
-    >
-      <h2>{{ album.title[$route.params.lang] }}</h2>
-
-      <div class="photo-container">
-        <img
-          class="lazyload"
-          v-for="(photo, index) in album.images"
-          :key="photo.imageName.name"
-          :alt="photo.imageName.name"
-          :data-src="`${imageUrlFor(photo.image)}`"
-          data-sizes="(min-width: 991px) 28vw, (min-width: 767px) 40vw, 100vw,"
-          :data-srcset="`${imageUrlFor(photo.image).width(300)} 300w,
-                    ${imageUrlFor(photo.image).width(600)} 600w,
-                    ${imageUrlFor(photo.image).width(800)} 800w,          
-                    ${imageUrlFor(photo.image).width(1200)} 1200w,          
-                    ${imageUrlFor(photo.image).width(1600)} 1600w,          
-                    ${imageUrlFor(photo.image).width(2000)} 2000w,          
-           `"
-          @click="openLightbox(index, albumIndex)"
-        />
-      </div>
-    </div>
+    <photo-album
+      v-for="(album, index) in photoAlbums"
+      :key="album.title.en"
+      :album="album"
+      :albumIndex="index"
+      @open-lightbox="openLightbox"
+    />
   </div>
 </template>
 
 <script>
 import Lightbox from "../components/Lightbox.vue";
+import PhotoAlbum from "../components/PhotoAlbum.vue";
 import sanity from "../sanity";
-import imageUrlBuilder from "@sanity/image-url";
 
-const imageBuilder = imageUrlBuilder(sanity);
 const query = `*[_type == "photoAlbums"]|order(orderRank)`;
 
 export default {
@@ -52,17 +32,13 @@ export default {
       albumIndex: null,
     };
   },
-  components: { Lightbox },
+  components: { Lightbox, PhotoAlbum },
   created() {
     this.fetchData().then((payload) => {
       this.photoAlbums = payload;
     });
   },
   methods: {
-    imageUrlFor(source) {
-      return imageBuilder.image(source);
-    },
-
     fetchData() {
       this.error = this.post = null;
 
@@ -76,6 +52,7 @@ export default {
       );
     },
     openLightbox(imageIndex, albumIndex) {
+      console.log("sef");
       this.imageIndex = imageIndex;
       this.albumIndex = albumIndex;
       this.lightboxOpen = true;
@@ -85,42 +62,4 @@ export default {
 </script>
 
 <style scoped lang="scss">
-@import ".././styles/mixins.scss";
-
-.album-container {
-  padding: 10px 0;
-  margin-bottom: 20px;
-}
-.album-inner-container {
-  display: flex;
-  flex-wrap: wrap;
-}
-.photo-container {
-  margin: 15px 10px;
-  display: flex;
-  flex-direction: row;
-  flex-wrap: wrap;
-  justify-content: center;
-  align-items: center;
-}
-img {
-  max-width: 100%;
-  margin: 10px 0;
-  object-fit: contain;
-  cursor: pointer;
-}
-
-@include atTabletPortrait {
-  img {
-    margin: 10px 10px;
-    width: 42%;
-  }
-}
-@include atDesktop {
-  img {
-    margin: 10px 20px;
-    width: auto;
-    height: 300px;
-  }
-}
 </style>
